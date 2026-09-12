8a2e84724b7c8f90d2df6e09207c61eb61d18f25
Ti Leggett <ti@daleggetts.com>
2026-09-12 09:50:29 -0500

    Initial commit

---
+++ src-disabled/ts/types.d.ts
@@ -0,0 +1,18 @@
+interface TtegglitTutorialReversiPlayer extends Player {
+    energy: number; // any information you add on each result['players']
+}
+
+interface TtegglitTutorialReversiGamedatas extends Gamedatas<TtegglitTutorialReversiPlayer> {
+    // Add here variables you set up in getAllDatas
+}
+   
+/*
+ * Describe here the types for your state args
+ */
+interface PlayerTurnArgs {
+    playableCardsIds: number[];
+}
+   
+/*
+ * Describe here the types for your notif args
+ */
